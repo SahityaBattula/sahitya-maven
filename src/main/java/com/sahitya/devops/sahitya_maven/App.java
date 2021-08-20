b2e5d27a1d1a74146e1fe8ea878eb93c1efdad4f
unknown <Sahitya>
2021-08-20 16:55:26 +0530

    initial checkin

---
+++ src/main/java/com/sahitya/devops/sahitya_maven/App.java
@@ -0,0 +1,13 @@
+package com.sahitya.devops.sahitya_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from sahitya" );
+    }
+}
